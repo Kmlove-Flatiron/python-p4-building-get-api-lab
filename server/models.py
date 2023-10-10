295f0ebf7e941904844fe8a20d11df8a8ad6bd8b
Kim Love <kimberlymlove15@gmail.com>
2023-10-09 20:25:06 -0400

    All tests passing

--- server/models.py
+++ server/models.py
@@ -12,9 +12,28 @@ class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # Relationships
+    baked_goods = db.relationship("BakedGood", back_populates="bakery", cascade="all, delete-orphan")
+
+    # Serialize Rules
+    serialize_rules = ("-baked_goods.bakery",)
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    bakery_id = db.Column(db.Integer, db.ForeignKey("bakeries.id"))
+
+    # Relationships
+    bakery = db.relationship("Bakery", back_populates="baked_goods")
+
+    # Serialize Rules
+    serialize_rules = ("-bakery.baked_goods",)
